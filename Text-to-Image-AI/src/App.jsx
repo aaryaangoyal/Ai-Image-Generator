06437ba5fba4459eba2b3d81c93a4d9f44c14a06
aryan goyal <aryangoyal329@gmail.com>
2026-07-14 21:18:57 +0530

    initial commit

---
+++ Text-to-Image-AI/src/App.jsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
+function App(){
+  return( 
+  <>
+  <h1 id='head'>Image Generator</h1>
+  <div>
+    <input></input>
+    <button><FontAwesomeIcon icon={byPrefixAndName.fas['arrow-up']} /></button>
+  </div>
+  </>
+  )
+}
+export default App
